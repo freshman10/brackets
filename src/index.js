2aaa408a9ff716f9fa7d47b67b6215dca816e742
freshman10 <70628469+freshman10@users.noreply.github.com>
2022-01-28 20:05:50 +0300

    fix: remove replaceAll

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function check(str, bracketsConfig) {
   while (str.length > 0){
     startLength = str.length;
     for (let pair of bracketsConfig){
-      str = str.replaceAll(pair[0] + pair[1],"");
+      str = str.replace(pair[0] + pair[1],"");
     }
     if (startLength === str.length){
       return false;
