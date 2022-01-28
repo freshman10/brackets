15a5a5a9562424190412aa86d8be1fbe2a532e68
freshman10 <70628469+freshman10@users.noreply.github.com>
2022-01-28 19:56:00 +0300

    init: load solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str.length > 0){
+    startLength = str.length
+    for (let pair of bracketsConfig){
+      //console.log(str, pair[0] + pair[1])
+      str = str.replaceAll(pair[0] + pair[1],"")
+      //console.log(str)
+
+    }
+    if (startLength === str.length){
+      return false
+    }
+  }
+  return true
 }
